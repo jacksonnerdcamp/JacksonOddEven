04a069416975f913b9efa2a32709cfec5d99a95a
BT_1N3_09@BKTECH.LOCAL <BT_1N3_09@BKTECH.LOCAL>
2018-09-18 08:45:21 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    public static String evenOdd(String str1){
+	    int x = 0
+        int c = 0
+        String output = ""
+            while(c < 3){
+                while(x < str1.length()){
+                    output = output + str.substring(x,x+1)
+                    x += 2
+                    }
+                }
+            }
+        }
+    }
+}
